be645de843b28925589b3f1df5c5c9dda4b29739
Karl Horning <k.horning@me.com>
2018-07-31 20:56:08 +0200

    Added query files

---
+++ lecture3/limit.sql
@@ -0,0 +1,1 @@
+SELECT * FROM flights LIMIT 2;
---
+++ lecture3/orderby.sql
@@ -0,0 +1,3 @@
+SELECT * FROM flights ORDER BY duration ASC;
+SELECT * FROM flights ORDER BY duration ASC LIMIT 3;
+SELECT * FROM flights ORDER BY duration DESC;
